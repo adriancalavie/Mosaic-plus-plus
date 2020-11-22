28f3be229a980a86abfd103972e66fa6d8df4728
Dinu Alin <dinui.alin@gmail.com>
2020-11-22 15:03:27 +0200

    Created resize and averageColor functions.|Testing them.

--- Mosaic++/Mosaic++/PictureTools.cpp
+++ Mosaic++/Mosaic++/PictureTools.cpp
@@ -5,9 +5,9 @@ Mat PictureTools::crop(Mat image, std::pair <uint8_t, uint8_t> topL, std::pair <
 {
 	uint8_t height = botR.first - topL.first;
 	uint8_t width = botR.second - topL.second;
-	Mat result(height,width, CV_8UC3);
+	Mat result(height, width, CV_8UC3);
 	std::cout << result.rows << " " << result.cols;
-	for (int index_rows = 0; index_rows < height ; index_rows++)
+	for (int index_rows = 0; index_rows < height; index_rows++)
 		for (int index_cols = 0; index_cols < width; index_cols++)
 		{
 			result.at<Vec3b>(index_rows, index_cols)[0] = image.at<Vec3b>(topL.first + index_rows, topL.second + index_cols)[0];
@@ -15,4 +15,52 @@ Mat PictureTools::crop(Mat image, std::pair <uint8_t, uint8_t> topL, std::pair <
 			result.at<Vec3b>(index_rows, index_cols)[2] = image.at<Vec3b>(topL.first + index_rows, topL.second + index_cols)[2];
 		}
 	return result;
+}
+
+Mat PictureTools::resize(const Mat& image, int width, int height)const
+{
+
+	Mat newimage(width, height, CV_8UC3);
+
+	int x_ratio = (int)((image.cols << 16) / width) + 1;
+	int y_ratio = (int)((image.rows << 16) / height) + 1;
+
+	int nearX, nearY;
+	for (int rows = 0; rows < height; rows++) {
+		for (int cols = 0; cols < width; cols++) {
+			nearX = ((cols * x_ratio) >> 16);
+			nearY = ((rows * y_ratio) >> 16);
+
+			newimage.at<cv::Vec3b>(rows, cols)[0] = image.at<cv::Vec3b>(nearY, nearX)[0];
+			newimage.at<cv::Vec3b>(rows, cols)[1] = image.at<cv::Vec3b>(nearY, nearX)[1];
+			newimage.at<cv::Vec3b>(rows, cols)[2] = image.at<cv::Vec3b>(nearY, nearX)[2];
+		}
+	}
+	return newimage;
+}
+
+Scalar PictureTools::averageColor(const Mat& image) const
+{
+	assert(!image.empty());
+	double blue, green, red;
+	blue = green = red = 0;
+	int size = (image.rows) * (image.cols);
+	for (int rows= 0; rows < image.rows; ++rows)
+	{
+		for (int cols = 0; cols < image.cols; ++cols)
+		{
+			blue+= (int)image.at<cv::Vec3b>(rows, cols)[0] / (double)100;
+			green += (int)image.at<cv::Vec3b>(rows, cols)[1] / (double)100;
+			red += (int)image.at<cv::Vec3b>(rows, cols)[2] / (double)100;
+
+		}
+	}
+	red = red/ size;
+	green = green / size;
+	blue= blue/ size;
+	red = red* 100;
+	green = green * 100;
+	blue= blue* 100;
+
+	return Scalar(blue, green, red);
 }
--- Mosaic++/Mosaic++/Source.cpp
+++ Mosaic++/Mosaic++/Source.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include "BasePictures.h"
+#include "PictureTools.h"
 int main()
 {
 	//std::cout << "Test1";
 	BasePictures test(10, ".jpg");
 	test.CreatingPicturesForMosaics();
+	waitKey(0);
 
+
+	/*TESTING RESIZE AND AVERGECOLOR FUNCTIONS
+	Mat image = imread("C:\\Users\\Dinu\\Desktop\\poze_de_baza\\16.jpg", IMREAD_COLOR);
+	assert(!image.empty());
+	PictureTools test2;
+	Mat image2 = Mat(200, 200, CV_8UC3, test2.averageColor(image));
+	Mat image3 = test2.resize(image, 900, 900);
+	imshow("Display Window1", image);
+	waitKey(0);
+	imshow("Display Window2", image2);
+	waitKey(0);
+	imshow("Display Window3", image3);
+	waitKey(0);
+	*/
 	return 0;
 }
