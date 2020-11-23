7597a1286c4596614a1b3ba02b357bc2c146468e
Radu <radubaciu218@yahoo.com>
2020-11-23 10:50:01 +0200

    Added image rotation functions to PictureTools.

--- Mosaic++/Mosaic++/PictureTools.h
+++ Mosaic++/Mosaic++/PictureTools.h
@@ -15,6 +15,9 @@ private:
 	static const uint8_t defaultSize = 100;
 public:
 	static Mat crop(const Mat& image, Point topL, Point botR);
+	static Mat rotateLeft(const Mat& image);
+	static Mat rotateRight(const Mat& image);
+	static Mat rotate180(const Mat& image);
 	static Mat resize(const Mat& image, const uint16_t& width = 0, const uint16_t& height = 0);
 	static Scalar averageColor(const Mat& image);
 	static Mat makeMosaic(const std::vector<Scalar>& dataPictures, Mat& image, const uint8_t& partitionSize = defaultSize);
