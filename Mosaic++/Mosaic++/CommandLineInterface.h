d239f03a00b0eb76d10bde0285e71edcc9577243
Adrian Calavie <adrian.calavie06@gmail.com>
2020-12-13 22:30:29 +0200

    Added switch statement to CLI constructor. Will complete with full logic. Part of a big CLI rework.

--- Mosaic++/Mosaic++/CommandLineInterface.h
+++ Mosaic++/Mosaic++/CommandLineInterface.h
@@ -4,9 +4,26 @@
 
 class CommandLineInterface
 {
+	enum class Command {
+		MOSAIC,
+		MOSAIC_H,
+		MOSAIC_HELP,
+		FOLDER,
+		FOLDER_H,
+		FOLDER_HELP,
+		COMPILE,
+		COMPILE_H,
+		COMPILE_HELP,
+		SELECTOR_TRIANGLE,
+		SELECTOR_RECTANGLE,
+		SELECTOR_H,
+		SELECTOR_HELP
+	};
+
+
 public:
 	CommandLineInterface(const std::string&);
 
-	static const std::map<std::string, uint8_t> vocabulary;
+	static const std::map<std::string, CommandLineInterface::Command> vocabulary;
 };
 
