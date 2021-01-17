a7732527eb5b051f746b5f360b59e5ceb0ba3f52
Adrian Calavie <adrian.calavie06@gmail.com>
2021-01-17 23:56:11 +0200

    Connected Mosaic to Qt GUI

--- MosaicQt/QtExample/main.cpp
+++ MosaicQt/QtExample/main.cpp
@@ -3,10 +3,29 @@
 
 #include <qdebug.h>
 
+#include "..\Mosaic\Mosaic.h"
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+	/*BasePictures test(5997);
+
+	test.AddPicturesMosaic(false);
+
+	cv::Mat input = cv::imread(Data::Defaults::PATH_TEST_IMAGE, cv::IMREAD_COLOR);
+
+	std::cout << input.rows << " " << input.cols << std::endl;
+	cv::Mat input3 = Mosaic::MakeMosaic(input, test, Method::RESIZING, Type::SQUARE, 30, false);
+	std::string str = Data::Defaults::PATH_RESULT_IMAGE + "Diamond.jpg";
+	cv::imwrite(str, input3);
+	cv::imshow("Processed Image", input3);*/
+	cv::Mat input = cv::imread("D:\\Mosaic++\\Mosaic++\\test.jpg", cv::IMREAD_COLOR);
+	input = PictureTools::resize(input, 300, 300, PictureTools::Algorithm::BILINEAR_INTERPOLATION);
+	cv::imshow("Photo", input);
+	cv::waitKey(0);
+
     return a.exec();
 }
