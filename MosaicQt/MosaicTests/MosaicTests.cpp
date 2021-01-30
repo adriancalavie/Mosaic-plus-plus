bf9365f79018afb18b8a29134cbf237fa94c6777
Adrian Calavie <adrian.calavie06@gmail.com>
2021-01-30 07:08:24 +0200

    Added Test project and some BIG mosaics
    Former-commit-id: 26f094a7125a6c9a50b23a77250a51509ee494fd

---
+++ MosaicQt/MosaicTests/MosaicTests.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MosaicTests
+{
+	TEST_CLASS(MosaicTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			//EMPTY
+		}
+
+		//TEST_METHOD()
+	};
+}
